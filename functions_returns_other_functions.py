a1f63dee3d8bd0268476d1bdaa5bd6c2b32d5e01
pravalika2222 <107530566+pravalika2222@users.noreply.github.com>
2022-08-10 19:26:50 +0530

    Add files via upload

---
+++ functions_returns_other_functions.py
@@ -0,0 +1,11 @@
+# def display():
+    # def message():
+        # return 'hello prava baby'
+        # return 'how are you prava'
+        # return 'i like your behaviour prava'
+    # return message
+# a=display()
+# print(a())
+a=25
+b=12
+print(a*b)
